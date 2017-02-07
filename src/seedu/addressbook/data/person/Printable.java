f74327ff62f235729572df386e42c11fb541823b
Yanzhang Shi <yanzhang.cgc@gmail.com>
2017-02-08 02:45:08 +0800

    Implements Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for generating a String for Name, Address, Phone and Email of a Person
+ *
+ */
+public interface Printable {
+	public abstract String getPrintableString();
+}
